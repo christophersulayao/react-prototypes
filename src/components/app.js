d631c5b4d512ba43f6518878f9517cf6e403f0b6
christophersulayao <christoper.sulayao@gmail.com>
2019-02-22 09:42:51 -0800

    need to re-do functional component prototypes, student data table isn't showing up

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,67 @@
+//
+// Import React
+// import React from 'react';
+// import Table from './table';
+//
+// // Create a functional component called App that
+// export default () => {
+//     //  returns a <h1> containing "My first React functional component"
+//     return (
+//         <h1>
+//             My first React functional component
+//         </h1>
+//     )
+// }
+
+// Import React
 import React from 'react';
 import Table from './table';
 
+// Create a functional component called App that
 export default () => {
-       const students = [
-              {
-                     name: 'Naruto Uzumaki',
-                     course: 'Ninjutsu',
-                     grade: 100
-              },
-              {
-                     name: 'Sasuke Uchiha',
-                     course: 'Emo',
-                     grade: 80
-              },
-              {
-                     name: 'Topher Uchiha',
-                     course: 'Sushi',
-                     grade: 99
-              }
-       ];
-       return (
-           <div className="container ">
-                  <h1>Student Grade Table</h1>
-                  <Table data={students}/>
-           </div>
-       )
+    //  returns a <h1> containing "My first React functional component"
+    return (
+        // Remove the <h1> element that is being returned from the component
+        //Add a new <div> with a class of container to the return statement
+        <div className="container">
+            {/*Inside the <div> add a new <h1> that says "Student Grade Table"*/}
+            <h1>
+                Student Grade Table
+            </h1>
+            {/*//Below the <h1> add the <Table> component*/}
+            <Table/>
+        </div>
+
+    )
 }
+import React from 'react';
+// import Table from './table';
+//
+// export default () => {
+//        const students = [
+//               {
+//                      name: 'Naruto Uzumaki',
+//                      course: 'Ninjutsu',
+//                      grade: 100
+//               },
+//               {
+//                      name: 'Sasuke Uchiha',
+//                      course: 'Emo',
+//                      grade: 80
+//               },
+//               {
+//                      name: 'Topher Uchiha',
+//                      course: 'Sushi',
+//                      grade: 99
+//               }
+//        ];
+//        return (
+//            <div className="container ">
+//                   <h1>Student Grade Table</h1>
+//                   <Table data={students}/>
+//            </div>
+//        )
+// }
 
 // import React from 'react';
 //
@@ -39,3 +75,33 @@ export default () => {
 //
 // }
 // export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- src/components/table.js
+++ src/components/table.js
@@ -1,55 +1,105 @@
-import React from 'react';
+// Import React at the top of the file
+
+
+// Create a functional component that returns a table
 
 export default () => {
     return (
-        <table className="table ">
+        // Add the class table to the <table> element
+        <table className="table">
+            {/*The table should have a <thead> section that has three headers*/}
+            {/*Name*/}
+            {/*Course*/}
+            {/*Grade*/}
+            {/*Add the class thead-inverse to the <thead> element*/}
             <thead className="thead-inverse">
-            <tr>
-                <th>Name</th>
-                <th>Course</th>
-                <th>Grade</th>
-            </tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
             </thead>
+
+            {/*In the <tbody> add three rows of student data based on the headers*/}
             <tbody>
-            <tr>
-                <td>Chris Sulayao</td>
-                <td>Growing a beard</td>
-                <td>0</td>
-            </tr>
-            <tr>
-                <td>Danika Quinteros</td>
-                <td>Helpful Helper</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>Alejandro Carillo</td>
-                <td>Pimpin</td>
-                <td>70</td>
-            </tr>
+
             </tbody>
         </table>
-    )
+    );
 }
-// function Table (){
-//     return(
-//         <table className="table">
-//             <thead className="thead-inverse">
-//                 <tr>
-//                     <th>Name</th>
-//                     <th>Course</th>
-//                     <th>Grade</th>
-//                 </tr>
-//             </thead>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
 //
+// export default () => {
+//     return (
+//         <table className="table ">
+//             <thead className="thead-inverse">
+//             <tr>
+//                 <th>Name</th>
+//                 <th>Course</th>
+//                 <th>Grade</th>
+//             </tr>
+//             </thead>
 //             <tbody>
-//                 <tr>
-//                     <th>Name</th>
-//                     <th>Course</th>
-//                     <th>Grade</th>
-//                 </tr>
+//             <tr>
+//                 <td>Chris Sulayao</td>
+//                 <td>Growing a beard</td>
+//                 <td>0</td>
+//             </tr>
+//             <tr>
+//                 <td>Danika Quinteros</td>
+//                 <td>Helpful Helper</td>
+//                 <td>100</td>
+//             </tr>
+//             <tr>
+//                 <td>Alejandro Carillo</td>
+//                 <td>Pimpin</td>
+//                 <td>70</td>
+//             </tr>
 //             </tbody>
 //         </table>
-//
-//     );
+//     )
 // }
-// export default Table;
+// // function Table (){
+// //     return(
+// //         <table className="table">
+// //             <thead className="thead-inverse">
+// //                 <tr>
+// //                     <th>Name</th>
+// //                     <th>Course</th>
+// //                     <th>Grade</th>
+// //                 </tr>
+// //             </thead>
+// //
+// //             <tbody>
+// //                 <tr>
+// //                     <th>Name</th>
+// //                     <th>Course</th>
+// //                     <th>Grade</th>
+// //                 </tr>
+// //             </tbody>
+// //         </table>
+// //
+// //     );
+// // }
+// // export default Table;
